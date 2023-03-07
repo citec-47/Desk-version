614785bc11a2a77bfb68767b4f1e76806a62dcda
citec-47 <mauricendonyi40@gmail.com>
2023-03-07 07:31:18 -0800

    adjustment of harmbugar menu

---
+++ main.js
@@ -0,0 +1,23 @@
+const statusbar = document.getElementById("statusbar");
+const navMenu = document.getElementById("home1");
+const closeMenu = document.getElementById("close");
+
+function mobileMenu() {
+  if (navMenu.style.display === "none") {
+    navMenu.style.display = "block";
+  } else {
+    navMenu.style.display = " none";
+  }
+}
+
+statusbar.addEventListener("click", mobileMenu);
+// close menc
+
+function menuClose() {
+  if (navMenu.style.display === "none") {
+    navMenu.style.display = "block";
+  } else {
+    navMenu.style.display = " none";
+  }
+}
+closeMenu.addEventListener("click", menuClose);
